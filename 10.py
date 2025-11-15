3b875b06896eb8bfa358ba4ba4f844c3ff25cb5b
bannu <ykkxnm4mtf@privaterelay.appleid.com>
2025-11-15 09:44:26 +0530

    Add files via upload

---
+++ 10.py
@@ -0,0 +1,19 @@
+import numpy as np
+from scipy.stats import chi2_contingency
+
+data = np.array([[207, 282], [241, 234], [242, 232]])
+stat, p, dof, expected = chi2_contingency(data)
+print("Chi-Square Stat:", stat)
+print("p-Value:", p)
+if p <= 0.05:
+    print("Dependent: Reject H0")
+else:
+    print("Independent: H0 holds true")
+
+"""
+OUTPUT:
+
+Chi-Square Stat: 9.49327943204954
+p-Value: 0.00868081625597216
+Dependent: Reject H0
+"""
